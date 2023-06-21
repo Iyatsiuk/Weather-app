ba40529809d4851a8d53bda1ce048b42b84cfca3
Iryna <iyatsiuk9924@gmail.com>
2023-06-21 23:21:58 +0300

    Update the time

--- src/app-logic.js
+++ src/app-logic.js
@@ -1,15 +1,46 @@
+function formatDate(timestamp) {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeatherData(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+
   let tempElement = document.querySelector("#temp");
   tempElement.innerHTML = Math.round(response.data.temperature.current);
+
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.temperature.humidity;
+  
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "e233dfo65c6ete669b0c1463eb14aaf0";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query=Paris&key=${apiKey}&units=metric`;
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=ternopil&key=${apiKey}&units=metric`;
 
 axios.get(apiURL).then(displayWeatherData);
